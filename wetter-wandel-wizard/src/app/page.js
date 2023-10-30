1fb6b7ce2d09c1d87b4ad253b716b4c54f6911ac
DerBoyderG <72962267+DerBoyderG@users.noreply.github.com>
2023-10-30 18:01:07 +0100

    "Hello World" Seite mit React und Tailwind css. Weitere Funktionen von Tailwind getestet.

--- wetter-wandel-wizard/src/app/page.js
+++ wetter-wandel-wizard/src/app/page.js
@@ -1,9 +1,33 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-<link rel="icon" href="/favicon.svg" type="image/svg+xml"></link>
+
+      {/* Favicon der Seite setzen */}
+      <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+
+      {/* Titel der Webseite festlegen */}
+      <title>Wetter Wandel Wizard</title>
+
+      {/* Überschrift der Webseite */}
+      <h1 className="text-6xl font-bold">Hello World</h1>
+
+      {/* Großes Logo der Webseite anzeigen */}
+      <Image src="/favicon.svg" alt="logo" width={500} height={500} />
+
+      {/* Begrüßungstexte */}
+      <p className="text-2xl font-bold">Willkommen bei Wetter Wandel Wizard</p>
+      <p className="text-sm">Viel Spaß!</p>
+
+      {/* Stilisierter Button */}
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Hier ist ein stilisierter Button
+      </button>
+
+      {/* Stilisierter Link zu einer externen Ressource */}
+      <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="text-blue-500 hover:text-blue-800">
+        Hier ist ein stilisierter Link
+      </a>
+
     </main>
   )
 }
