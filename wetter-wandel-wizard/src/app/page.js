f3ab034f9b064972de7fb0044008cd473bdf08a9
DerBoyderG <72962267+DerBoyderG@users.noreply.github.com>
2023-11-20 11:33:24 +0100

    currentTime funktion eingebraut damit die urhzeit angepasst wird

--- wetter-wandel-wizard/src/app/page.js
+++ wetter-wandel-wizard/src/app/page.js
@@ -1,23 +1,38 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import Header from "./Components/Header";
 
 export default function Home() {
+  const [currentTime, setCurrentTime] = useState(
+    new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+  );
+
+  useEffect(() => {
+    // Timer einrichten, um die aktuelle Uhrzeit jede Minute zu aktualisieren
+    const timerId = setInterval(() => {
+      setCurrentTime(
+        new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+      );
+    }, 60000); // aktualisiert jede Minute
+
+    // Aufräumfunktion, die den Timer löscht, wenn die Komponente unmountet wird
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []); // Leeres Array bedeutet, dass dieser Effekt nur beim Mounten und Unmounten läuft
 
   const tabs = [
     { title: 'Heute', href: '#heute' },
     { title: 'Klimawandel', href: '#klimawandel' },
   ];
 
-  const currentTime = '21:00 Uhr';
-
-
   return (
-      <div>
-        <Header currentTime={currentTime} tabs={tabs} />
-        {/* Rest der Komponenten */}
-      </div>
-    );
-  };
+    <div>
+      <Header currentTime={currentTime} tabs={tabs} />
+      {/* Rest der Komponenten */}
+    </div>
+  );
+};
+
   
